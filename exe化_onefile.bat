3bff74002147dd3a7fcb33bef67aea4b3cc3789c
yt3trees <y.atforest@gmail.com>
2021-10-24 00:42:24 +0900

    update

--- exe化_onefile.bat
+++ exe化_onefile.bat
@@ -1,3 +1,3 @@
 rem upx --upx-dir C:\upx-3.96-win64
-pyinstaller Copipeditor.py --name Copipeditor --noconsole --onefile --icon=icon/copytool.ico
+pyinstaller Copipeditor.py --name Copipeditor --onefile --icon=icon/copytool.ico
 pause
